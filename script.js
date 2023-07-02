c21a41756da7ad0d470b0447f5da2be1b4e8b1fc
Genesis Lara <genesislara@pursuit.org>
2023-07-01 20:53:04 -0400

    create and remove added

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const form = document.querySelector("form");
+const ul = document.createElement('ul');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    const li = document.createElement("li");
+    const candleListing = event.target
+    const candleName = document.createElement("h2");
+    const candlePrice = document.createElement("h4");
+    const candleImg = document.createElement("img");
+    const remove = document.createElement("button")
+
+    candleName.textContent = candleListing.name.value;
+    li.append(candleName,candlePrice,candleImg, remove);
+    ul.append(li)
+
+    remove.innerText = "Remove Candle"
+    
+    
+    remove.addEventListener("click", (event) => {
+        console.log('clicked!')
+        li.remove();
+    })
 })
+
+document.body.append(ul)
+
+// let formData = {
+    //         name: event.target.name.value,
+    //         scent: event.target.scent.value,
+    //         price: event.target.price.value,
+    //         img: event.target.img.value,
+    //         size: event.target.size.value,
+    //         inStock: event.target.inStock.value,
+    //     }
+    
+    
+    
