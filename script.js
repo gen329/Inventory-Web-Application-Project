3bfde6144beda62c64030b57c4940cb8d76ef325
Genesis Lara <genesislara@pursuit.org>
2023-07-20 18:22:49 -0400

    fixes new candle feature

--- script.js
+++ script.js
@@ -3,108 +3,76 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const formData = {
-        name: event.target.name.value,
-        scent: event.target.scent.value,
-        price: event.target.price.value,
-        img: event.target.img.value,
-        size: event.target.size.value,
-        inStock: event.target.inStock.value,
-    };
+    let name = event.target.name.value;
+    let scent = event.target.scent.value;
+    let price = event.target.price.value;
+    let img = event.target.img.value;
+    let size = event.target.size.value;
+    let inStock = event.target.inStock.value;
 
-    const candleOne = document.querySelector("article.candle1")
-    const removeButtonOne = document.querySelector("button.candle1")
-    const inStockCandles = document.querySelector("section.inStockCandles")
-    removeButtonOne.addEventListener("click", (event) => {
-        {
-            candleOne.remove()
-        }
-        return inStockCandles;
-    })
-
-    const candleTwo = document.querySelector("article.candle2")
-    const removeButtonTwo = document.querySelector("button.candle2")
-    removeButtonTwo.addEventListener("click", (event) => {
-        {
-            candleTwo.remove()
-        }
-        return inStockCandles;
-    })
-    const candleThree = document.querySelector("article.candle3")
-    const removeButtonThree = document.querySelector("button.candle3")
-    removeButtonThree.addEventListener("click", (event) => {
-        {
-            candleThree.remove()
-        }
-        return inStockCandles;
-    })
-
-
-    function generateNewCandle(name, scent, price, size, img, inStock) {
-        const article = newCandle(name, scent, price, size, img, inStock);
-        const div = document.querySelector("article");
-        div.append(article);
-    }
-    generateNewCandle(name, scent, price, img, size, inStock);
+    newCandle(name, scent, price, img, size, inStock);
     form.reset();
 });
 
 function newCandle(name, scent, price, size, img, inStock) {
     const article = document.createElement("article");
-
+    const div = document.querySelector("article");
+    div.append(article);
     if (name) {
         const h2 = document.createElement("h2");
         h2.textContent += name;
         article.append(h2);
     }
-
     if (scent) {
         const h3 = document.createElement("h3");
         h3.textContent += scent;
         article.append(h3);
     }
-
     if (price) {
         const p = document.createElement("p");
         p.textContent += price;
         article.append(p);
     }
-
     if (img) {
         const img = document.createElement("img");
         img.src += img;
         article.append(img);
     }
-
     if (size) {
-        const size = document.createElement("h4");
+        const h4 = document.createElement("h4");
         h4.textContent += size;
         article.append(h4);
     }
-
     if (inStock) {
-        const div = doucment.createElement("div");
+        const div = document.createElement("div");
         div.textContent += "";
         article.append(div);
     }
 }
 
 function removeCandle() {
-    const remove = document.createElement("button");
-    
-    candle = document.querySelector("article");
-    article.append(remove);
+    const remove = document.createElement("#button");
+    const article = document.querySelector("article");
+    remove.innerText = "Remove";
+    article.append(remove)
 
-    remove.addEventListener("click", (event) => {
-        candle.remove();
+
+    const removeButtons = document.querySelector
+    for (let button of removeButtons) {
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            event.target.parentElement.remove();
+            event.reset()
+        });
+    }
+    removeButtons.addEventListener("click", (event) => {
+        ul.remove();
     });
-
-remove.innerText = "Remove";
-article.append(remove);
-
-remove.addEventListener("click", (event) => {
-    article.remove();
-});
-return removedCandle;
-
+}
+function toggleInStock(element) {
+    element.addEventListener("click", (event) => {
+        element.parentNode.classList.toggle("In Stock");
+        let innerButtonText = element.innerText === "In Stock" ? "Out of Stock"
+    element.innerText = innerButtonText;
+    }));
 }
